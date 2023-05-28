048f2d8823b7b4946890a4e642752e4ceb8ea7b0
Emma2947 <emmakulriksen@gmail.com>
2023-05-28 09:00:48 +0200

    indsat kontakt fra e

--- Programmering/js/kontakt.js
+++ Programmering/js/kontakt.js
@@ -1,46 +1,46 @@
 //Javascript til Burgermenuen
 
 function myFunction() {
-    var x = document.getElementById("menu");
-    var y = document.getElementById("topnav");
-    if (x.style.display === "block") {
-     x.style.display = "none";
-     } else {
-      x.style.display = "block";
-     }
-    }
+  var x = document.getElementById("menu");
+  var y = document.getElementById("topnav");
+  if (x.style.display === "block") {
+   x.style.display = "none";
+   } else {
+    x.style.display = "block";
+   }
+  }
 
 
 //Skrifttype
 
-  (function(d) {
-    var config = {
-      kitId: 'uex2dst',
-      scriptTimeout: 3000,
-      async: true
-    },
-    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-  })(document);
+(function(d) {
+  var config = {
+    kitId: 'uex2dst',
+    scriptTimeout: 3000,
+    async: true
+  },
+  h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+})(document);
 
 
 
 //hvad der sker efter send knap
-  function submitForm(event) {
-    event.preventDefault(); // Forhindrer formular i at blive indsendt og siden genindlæses
-    
-    // Udfør eventuelle yderligere handlinger eller valideringer her
-    
-    // Send brugeren videre til en ny side
-    window.location.href = "conformation.html";
-  }
+function submitForm(event) {
+  event.preventDefault(); // Forhindrer formular i at blive indsendt og siden genindlæses
+  
+  // Udfør eventuelle yderligere handlinger eller valideringer her
+  
+  // Send brugeren videre til en ny side
+  window.location.href = "conformation.html";
+}
 
 
-  function submitForm(event) {
-    event.preventDefault(); // Forhindrer formular i at blive indsendt og siden genindlæses
-    
-    // Udfør eventuelle yderligere handlinger eller valideringer her
-    
-    // Vis linket til den nye side
-    var linkElement = document.getElementById('link');
-    linkElement.classList.remove('hidden');
-  }
+function submitForm(event) {
+  event.preventDefault(); // Forhindrer formular i at blive indsendt og siden genindlæses
+  
+  // Udfør eventuelle yderligere handlinger eller valideringer her
+  
+  // Vis linket til den nye side
+  var linkElement = document.getElementById('link');
+  linkElement.classList.remove('hidden');
+}
